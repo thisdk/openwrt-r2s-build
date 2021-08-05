31371e528dc65a0ee3f36588dafbce7e736a4af6
Qixiong Yuan <thisdk@gmail.com>
2021-08-06 00:42:36 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,6 +8,25 @@ git clone -b openwrt-21.02 https://git.openwrt.org/openwrt/openwrt.git
 
 cd openwrt && sed -i 's/-SNAPSHOT/.1/g' include/version.mk
 
+# wan / lan
+
+sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+
+# 16384 / 65535
+
+sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+
+# BBRv2
+
+wget -qO - https://raw.githubusercontent.com/QiuSimons/R2S-R4S-X86-OpenWrt/master/PATCH/BBRv2/openwrt-kmod-bbr2.patch | patch -p1
+wget https://raw.githubusercontent.com/QiuSimons/R2S-R4S-X86-OpenWrt/master/PATCH/BBRv2/693-Add_BBRv2_congestion_control_for_Linux_TCP.patch -O target/linux/generic/hack-5.4/693-Add_BBRv2_congestion_control_for_Linux_TCP.patch
+wget -qO - https://github.com/openwrt/openwrt/commit/cfaf039.patch | patch -p1
+
+# ARMv8 AES
+sed -i 's,-mcpu=generic,-mcpu=cortex-a53+crypto,g' include/target.mk
+wget https://raw.githubusercontent.com/QiuSimons/R2S-R4S-X86-OpenWrt/master/PATCH/mbedtls/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch -O package/libs/mbedtls/patches/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch
+
 # clone openwrt plugin source
 
 ./scripts/feeds update -a && ./scripts/feeds install -a
