978a37013f667cc2889870f3b0eb6fa2b4fb440a
Qixiong Yuan <thisdk@gmail.com>
2021-08-06 15:06:27 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,9 +20,9 @@ sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
 # BBRv2
 
-wget -qO - https://raw.githubusercontent.com/QiuSimons/R2S-R4S-X86-OpenWrt/master/PATCH/BBRv2/openwrt-kmod-bbr2.patch | patch -p1
+wget -qO- https://raw.githubusercontent.com/QiuSimons/R2S-R4S-X86-OpenWrt/master/PATCH/BBRv2/openwrt-kmod-bbr2.patch | patch -p1
 wget https://raw.githubusercontent.com/QiuSimons/R2S-R4S-X86-OpenWrt/master/PATCH/BBRv2/693-Add_BBRv2_congestion_control_for_Linux_TCP.patch -O target/linux/generic/hack-5.4/693-Add_BBRv2_congestion_control_for_Linux_TCP.patch
-wget -qO - https://github.com/openwrt/openwrt/commit/cfaf039.patch | patch -p1
+wget -qO- https://github.com/openwrt/openwrt/commit/cfaf039.patch | patch -p1
 
 # ARMv8 AES
 sed -i 's,-mcpu=generic,-mcpu=cortex-a53+crypto,g' include/target.mk
@@ -31,10 +31,8 @@ wget https://raw.githubusercontent.com/QiuSimons/R2S-R4S-X86-OpenWrt/master/PATC
 # CPU Info
 wget -P target/linux/generic/hack-5.4/ https://github.com/immortalwrt/immortalwrt/raw/master/target/linux/generic/hack-5.4/312-arm64-cpuinfo-Add-model-name-in-proc-cpuinfo-for-64bit-ta.patch
 
-### Fullcone-NAT 部分 ###
-pushd target/linux/generic/hack-5.4
-wget https://github.com/immortalwrt/immortalwrt/raw/master/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
-popd
+# FullConeNAT
+wget -P target/linux/generic/hack-5.4 https://github.com/immortalwrt/immortalwrt/raw/master/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
 mkdir package/network/config/firewall/patches
 wget -P package/network/config/firewall/patches/ https://github.com/immortalwrt/immortalwrt/raw/master/package/network/config/firewall/patches/fullconenat.patch
 wget -qO- https://github.com/msylgj/R2S-R4S-OpenWrt/raw/21.02/SCRIPTS/fix_firewall_flock.patch | patch -p1
@@ -47,12 +45,12 @@ cp -rf ../openwrt-lienol/package/network/fullconenat ./package/network/fullconen
 
 # copy build file and config
 
-cp ../.config .config
+# cp ../.config .config
 
 # openwrt build dependencies
 
-make defconfig && make download -j8
+# make defconfig && make download -j8
 
 # make openwrt source
 
-make -j4
+# make -j4
