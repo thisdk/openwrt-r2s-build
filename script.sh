7d7647ae7bfaac5c71a20653bbd5b870dfde0e07
Qixiong Yuan <thisdk@gmail.com>
2021-08-06 15:23:06 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,10 @@
 # clone openwrt
 
 git clone -b openwrt-21.02 https://git.openwrt.org/openwrt/openwrt.git
-git clone -b main --depth 1 https://github.com/Lienol/openwrt.git openwrt-lienol
+git clone -b main https://github.com/Lienol/openwrt.git openwrt-lienol
+git clone -b master https://github.com/immortalwrt/immortalwrt.git openwrt-immortalwrt-
+git clone -b master https://github.com/immortalwrt/packages.git immortalwrt-packages
+git clone -b master https://github.com/coolsnowwolf/lede.git openwrt-lede
 
 # version replace
 
@@ -18,6 +21,10 @@ sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/
 
 sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
+# clone openwrt plugin source
+
+./scripts/feeds update -a && ./scripts/feeds install -a
+
 # BBRv2
 
 wget -qO- https://raw.githubusercontent.com/QiuSimons/R2S-R4S-X86-OpenWrt/master/PATCH/BBRv2/openwrt-kmod-bbr2.patch | patch -p1
@@ -25,13 +32,16 @@ wget https://raw.githubusercontent.com/QiuSimons/R2S-R4S-X86-OpenWrt/master/PATC
 wget -qO- https://github.com/openwrt/openwrt/commit/cfaf039.patch | patch -p1
 
 # ARMv8 AES
+
 sed -i 's,-mcpu=generic,-mcpu=cortex-a53+crypto,g' include/target.mk
 wget https://raw.githubusercontent.com/QiuSimons/R2S-R4S-X86-OpenWrt/master/PATCH/mbedtls/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch -O package/libs/mbedtls/patches/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch
 
 # CPU Info
+
 wget -P target/linux/generic/hack-5.4/ https://github.com/immortalwrt/immortalwrt/raw/master/target/linux/generic/hack-5.4/312-arm64-cpuinfo-Add-model-name-in-proc-cpuinfo-for-64bit-ta.patch
 
 # FullConeNAT
+
 wget -P target/linux/generic/hack-5.4 https://github.com/immortalwrt/immortalwrt/raw/master/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
 mkdir package/network/config/firewall/patches
 wget -P package/network/config/firewall/patches/ https://github.com/immortalwrt/immortalwrt/raw/master/package/network/config/firewall/patches/fullconenat.patch
@@ -39,9 +49,20 @@ wget -qO- https://github.com/msylgj/R2S-R4S-OpenWrt/raw/21.02/SCRIPTS/fix_firewa
 patch -p1 < ../PATCH/firewall/luci-app-firewall_add_fullcone.patch
 cp -rf ../openwrt-lienol/package/network/fullconenat ./package/network/fullconenat
 
-# clone openwrt plugin source
+# AutoCore
 
-./scripts/feeds update -a && ./scripts/feeds install -a
+rm -rf feeds/packages/utils/coremark
+cp -rf ../immortalwrt-packages/utils/coremark feeds/packages/utils/coremark
+cp -rf ../openwrt-immortalwrt/package/emortal/autocore package/lean/autocore
+
+# KMS
+
+cp -rf ../openwrt-lede/package/lean/luci-app-vlmcsd package/lean/luci-app-vlmcsd
+cp -rf ../openwrt-lede/package/lean/vlmcsd package/lean/vlmcsd
+
+# OpenClash
+
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 
 # copy build file and config
 
